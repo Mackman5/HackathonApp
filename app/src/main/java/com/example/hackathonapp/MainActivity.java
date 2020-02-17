4d18d1d314a9bae606711971fb0a7f27eb9fd194
SebasTheFish <sebastianq04@gmail.com>
2020-02-17 15:31:15 -0800

    Big boi push

--- app/src/main/java/com/example/hackathonapp/MainActivity.java
+++ app/src/main/java/com/example/hackathonapp/MainActivity.java
@@ -1,13 +1,25 @@
 package com.example.hackathonapp;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
+import android.widget.TextView;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -57,6 +69,60 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(startIntent);
             }
         });
+
+
+        LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View v = inflater.inflate(R.layout.activity_main, null);
+
+
+        // Find the ScrollView
+        ScrollView sv = (ScrollView) v.findViewById(R.id.scrollView2);
+
+        // Create a LinearLayout element
+        final LinearLayout ll = new LinearLayout(this);
+        ll.setOrientation(LinearLayout.VERTICAL);
+
+        final FirebaseDatabase database = FirebaseDatabase.getInstance();
+
+        DatabaseReference myRef = database.getReference("news1");
+
+        // Read from the database
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                List<String> keys = new ArrayList<>();
+                for(DataSnapshot keyNode: dataSnapshot.getChildren())
+                {
+                    keys.add(keyNode.getKey());
+                }
+                List<String> values  = new ArrayList<>();
+                for(Integer i = 0; i < keys.size(); i++)
+                {
+                    values.add(dataSnapshot.child(keys.get(i)).getValue().toString());
+                }
+                // Add text
+                TextView tv = new TextView(getApplicationContext());
+                tv.setText(values.get(1));
+                ll.addView(tv);
+                TextView tv2 = new TextView(getApplicationContext());
+                tv2.setText(values.get(0));
+                ll.addView(tv2);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                System.out.println("DDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDED");
+                // Failed to read value
+            }
+        });
+
+        // Add the LinearLayout element to the ScrollView
+        sv.addView(ll);
+
+        // Display the view
+        setContentView(v);
     }
 
     public static void write(String ref, String value){
--- app/src/main/java/com/example/hackathonapp/reportnews.java
+++ app/src/main/java/com/example/hackathonapp/reportnews.java
@@ -1,14 +1,22 @@
 package com.example.hackathonapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.webkit.WebView;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 public class reportnews extends AppCompatActivity {
 
@@ -26,17 +34,95 @@ public class reportnews extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 String headline = headlineInput.getText().toString(),
-                       announcement = announcementInput.getText().toString();
+                        announcement = announcementInput.getText().toString();
 
-                write("news/headline",headline);
-                write("news/announcement",announcement);
+                if (headline == "" || announcement == "")
+                {
+                    Toast toast =Toast.makeText(getApplicationContext(), "Please enter valid Headline and Announcement", Toast.LENGTH_SHORT);
+                    toast.show();
+                }
+                else
+                {
+                    final FirebaseDatabase database = FirebaseDatabase.getInstance();
+
+                    DatabaseReference myRef = database.getReference("news2");
+
+                    // Read from the database
+                    myRef.addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(DataSnapshot dataSnapshot) {
+                            // This method is called once with the initial value and again
+                            // whenever data at this location is updated.
+                            List<String> keys = new ArrayList<>();
+                            for(DataSnapshot keyNode: dataSnapshot.getChildren())
+                            {
+                                keys.add(keyNode.getKey());
+                            }
+                            List<String> values  = new ArrayList<>();
+                            for(Integer i = 0; i < keys.size(); i++)
+                            {
+                                values.add(dataSnapshot.child(keys.get(i)).getValue().toString());
+                            }
+                            write("news3/headline",values.get(1));
+                            write("news3/announcement",values.get(0));
+                        }
+
+                        @Override
+                        public void onCancelled(DatabaseError error) {
+                            System.out.println("DDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDED");
+                            // Failed to read value
+                        }
+                    });
+
+
+                    myRef = database.getReference("news1");
+
+                    // Read from the database
+                    myRef.addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(DataSnapshot dataSnapshot) {
+                            // This method is called once with the initial value and again
+                            // whenever data at this location is updated.
+                            List<String> keys = new ArrayList<>();
+                            for(DataSnapshot keyNode: dataSnapshot.getChildren())
+                            {
+                                keys.add(keyNode.getKey());
+                            }
+                            List<String> values  = new ArrayList<>();
+                            for(Integer i = 0; i < keys.size(); i++)
+                            {
+                                values.add(dataSnapshot.child(keys.get(i)).getValue().toString());
+                            }
+                            write("news2/headline",values.get(1));
+                            write("news2/announcement",values.get(0));
+                        }
+
+                        @Override
+                        public void onCancelled(DatabaseError error) {
+                            System.out.println("DDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDED");
+                            // Failed to read value
+                        }
+                    });
+                    try
+                    {
+                        TimeUnit.SECONDS.sleep(3);
+                        write("news1/headline", headline);
+                        write("news1/announcement",announcement);
+                    }
+                    catch(Exception e)
+                    {
+                        System.out.println(e);
+                    }
+                }
             }
         });
     }
     //FIREBASE WRITE
-    public static void write(String ref, String value){
+    public static void write(String ref, String value) {
         FirebaseDatabase database = FirebaseDatabase.getInstance();
         DatabaseReference myRef = database.getReference(ref);
         myRef.setValue(value);
     }
-}
+
+    }
+
