7c245ce19ebafdc1b2895ac822ed007b3d73e8a9
SebasTheFish <sebastianq04@gmail.com>
2020-02-17 06:58:42 -0800

    Homepage

--- app/src/main/java/com/example/hackathonapp/report.java
+++ app/src/main/java/com/example/hackathonapp/report.java
@@ -1,7 +1,10 @@
 package com.example.hackathonapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class report extends AppCompatActivity {
 
@@ -9,5 +12,24 @@ public class report extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_report);
+
+        Button reportnewsBtn = findViewById(R.id.reportnewsBtn),
+               reportmapBtn = findViewById(R.id.mapreportBtn);
+
+        reportnewsBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent startIntent = new Intent(getApplicationContext(), reportnews.class);
+                startActivity(startIntent);
+            }
+        });
+        reportmapBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent startIntent = new Intent(getApplicationContext(), reportmap.class);
+                startActivity(startIntent);
+            }
+        });
     }
+
 }
---
+++ app/src/main/java/com/example/hackathonapp/reportmap.java
@@ -0,0 +1,13 @@
+package com.example.hackathonapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class reportmap extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reportmap);
+    }
+}
