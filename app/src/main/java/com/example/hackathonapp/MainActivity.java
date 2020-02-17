8967db78d11f7c3e2ed344f424707f2ffaaee1c3
SebasTheFish <sebastianq04@gmail.com>
2020-02-17 06:53:12 -0800

    Homepage

--- app/src/main/java/com/example/hackathonapp/MainActivity.java
+++ app/src/main/java/com/example/hackathonapp/MainActivity.java
@@ -2,10 +2,13 @@ package com.example.hackathonapp;
 
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.content.Intent;
 import android.os.Build;
 import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
@@ -17,8 +20,39 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Write a message to the database
-        write("test", "hi");
+        Button thingstodoBtn = findViewById(R.id.thingstodoBtn),
+               aboutBtn = findViewById(R.id.aboutBtn),
+               mapBtn = findViewById(R.id.mapBtn),
+               reportBtn = findViewById(R.id.reportBtn);
+
+        thingstodoBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent startIntent = new Intent(getApplicationContext(), thingstodo.class);
+                startActivity(startIntent);
+            }
+        });
+        aboutBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent startIntent = new Intent(getApplicationContext(), about.class);
+                startActivity(startIntent);
+            }
+        });
+        mapBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent startIntent = new Intent(getApplicationContext(), map.class);
+                startActivity(startIntent);
+            }
+        });
+        reportBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent startIntent = new Intent(getApplicationContext(), report.class);
+                startActivity(startIntent);
+            }
+        });
     }
 
     public static void write(String ref, String value){
