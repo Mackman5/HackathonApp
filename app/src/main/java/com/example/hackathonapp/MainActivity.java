b39151b72500de4258851b461cf237eb8bdade2f
mmell <mmellary@gmail.com>
2020-02-16 20:51:21 -0500

    firebase writing wow

--- app/src/main/java/com/example/hackathonapp/MainActivity.java
+++ app/src/main/java/com/example/hackathonapp/MainActivity.java
@@ -1,7 +1,14 @@
 package com.example.hackathonapp;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.os.Build;
+import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +16,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Write a message to the database
+        write("test", "hi");
+    }
+
+    public static void write(String ref, String value){
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference(ref);
+        myRef.setValue(value);
     }
 }
